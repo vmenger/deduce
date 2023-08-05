4163057aa552efe3851513c7b05873d09ffc3645
Vincent Menger <vmenger@protonmail.com>
2023-08-05 13:22:37 +0200

    Update pipeline test

--- tests/pipeline/test_deduce.py
+++ tests/pipeline/test_deduce.py
@@ -4,29 +4,31 @@ from deduce import Deduce
 from deduce.person import Person
 
 text = (
-    "Dit is stukje tekst met daarin de naam Jan Jansen. De patient J. Jansen "
-    "(e: j.jnsen@email.com, t: 06-12345678) is 64 jaar oud en woonachtig in Utrecht. Hij werd op 10 "
-    "oktober 2018 door arts Peter de Visser ontslagen van de kliniek van het UMCU."
+    "betreft: Jan Jansen, bsn 111222333, patnr 000334433. De patient J. Jansen is 64 jaar oud en woonachtig in "
+    "Utrecht. Hij werd op 10 oktober 2018 door arts Peter de Visser ontslagen van de kliniek van het UMCU. "
+    "Voor nazorg kan hij worden bereikt via j.JNSEN.123@gmail.com of (06)12345678."
 )
 
 
 class TestDeduce:
     def test_annotate(self):
         deduce = Deduce()
-        person = Person(first_names=["Jan"], surname="Jansen")
-        doc = deduce.deidentify(text, metadata={"patient": person})
+        metadata = {"patient": Person(first_names=["Jan"], surname="Jansen")}
+        doc = deduce.deidentify(text, metadata=metadata)
 
         expected_annotations = dd.AnnotationSet(
             [
-                dd.Annotation("Jan Jansen", 39, 49, "patient"),
-                dd.Annotation("patient J. Jansen", 54, 71, "patient"),
-                dd.Annotation("j.jnsen@email.com", 76, 93, "email"),
-                dd.Annotation("06-12345678", 98, 109, "telefoonnummer"),
-                dd.Annotation("64", 114, 116, "leeftijd"),
-                dd.Annotation("Utrecht", 143, 150, "locatie"),
-                dd.Annotation("10 oktober 2018", 164, 179, "datum"),
-                dd.Annotation("Peter de Visser", 190, 205, "persoon"),
-                dd.Annotation("UMCU", 239, 243, "instelling"),
+                dd.Annotation(text="(06)12345678", start_char=272, end_char=284, tag="telefoonnummer"),
+                dd.Annotation(text="111222333", start_char=25, end_char=34, tag="bsn"),
+                dd.Annotation(text="Peter de Visser", start_char=153, end_char=168, tag="persoon"),
+                dd.Annotation(text="j.JNSEN.123@gmail.com", start_char=247, end_char=268, tag="email"),
+                dd.Annotation(text="patient J. Jansen", start_char=56, end_char=73, tag="patient"),
+                dd.Annotation(text="Jan Jansen", start_char=9, end_char=19, tag="patient"),
+                dd.Annotation(text="10 oktober 2018", start_char=127, end_char=142, tag="datum"),
+                dd.Annotation(text="64", start_char=77, end_char=79, tag="leeftijd"),
+                dd.Annotation(text="000334433", start_char=42, end_char=51, tag="id"),
+                dd.Annotation(text="Utrecht", start_char=106, end_char=113, tag="locatie"),
+                dd.Annotation(text="UMCU", start_char=202, end_char=206, tag="instelling"),
             ]
         )
 
@@ -34,30 +36,29 @@ class TestDeduce:
 
     def test_deidentify(self):
         deduce = Deduce()
-        person = Person(first_names=["Jan"], surname="Jansen")
-        doc = deduce.deidentify(text, metadata={"patient": person})
+        metadata = {"patient": Person(first_names=["Jan"], surname="Jansen")}
+        doc = deduce.deidentify(text, metadata=metadata)
 
         expected_deidentified = (
-            "Dit is stukje tekst met daarin de naam <PATIENT>. De <PATIENT> (e: <EMAIL-1>, t: <TELEFOONNUMMER-1>) "
-            "is <LEEFTIJD-1> jaar oud en woonachtig in <LOCATIE-1>. Hij werd op <DATUM-1> door arts <PERSOON-1> "
-            "ontslagen van de kliniek van het <INSTELLING-1>."
+            "betreft: <PATIENT>, bsn <BSN-1>, patnr <ID-1>. De <PATIENT> is <LEEFTIJD-1> jaar oud en woonachtig in "
+            "<LOCATIE-1>. Hij werd op <DATUM-1> door arts <PERSOON-1> ontslagen van de kliniek van het <INSTELLING-1>. "
+            "Voor nazorg kan hij worden bereikt via <EMAIL-1> of <TELEFOONNUMMER-1>."
         )
 
         assert doc.deidentified_text == expected_deidentified
 
     def test_annotate_intext(self):
         deduce = Deduce()
-        person = Person(first_names=["Jan"], surname="Jansen")
-        doc = deduce.deidentify(text, metadata={"patient": person})
-
-        print(dd.utils.annotate_intext(doc))
+        metadata = {"patient": Person(first_names=["Jan"], surname="Jansen")}
+        doc = deduce.deidentify(text, metadata=metadata)
 
         expected_intext_annotated = (
-            "Dit is stukje tekst met daarin de naam <PATIENT>Jan Jansen</PATIENT>. "
-            "De <PATIENT>patient J. Jansen</PATIENT> (e: <EMAIL>j.jnsen@email.com</EMAIL>, "
-            "t: <TELEFOONNUMMER>06-12345678</TELEFOONNUMMER>) is <LEEFTIJD>64</LEEFTIJD> jaar oud "
-            "en woonachtig in <LOCATIE>Utrecht</LOCATIE>. Hij werd op <DATUM>10 oktober 2018</DATUM> door "
-            "arts <PERSOON>Peter de Visser</PERSOON> ontslagen van de kliniek van het <INSTELLING>UMCU</INSTELLING>."
+            "betreft: <PATIENT>Jan Jansen</PATIENT>, bsn <BSN>111222333</BSN>, patnr <ID>000334433</ID>. "
+            "De <PATIENT>patient J. Jansen</PATIENT> is <LEEFTIJD>64</LEEFTIJD> jaar oud en woonachtig in "
+            "<LOCATIE>Utrecht</LOCATIE>. Hij werd op <DATUM>10 oktober 2018</DATUM> door arts "
+            "<PERSOON>Peter de Visser</PERSOON> ontslagen van de kliniek van het <INSTELLING>UMCU</INSTELLING>. "
+            "Voor nazorg kan hij worden bereikt via <EMAIL>j.JNSEN.123@gmail.com</EMAIL> of "
+            "<TELEFOONNUMMER>(06)12345678</TELEFOONNUMMER>."
         )
 
         assert dd.utils.annotate_intext(doc) == expected_intext_annotated
