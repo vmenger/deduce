03c706dfc6eee85d57b36379aa828640b374b757
Lydia Mennes <lydia.mennes@nedap.com>
2024-01-18 11:23:27 +0100

    docformatter

--- deduce/processor_loader.py
+++ deduce/processor_loader.py
@@ -302,7 +302,7 @@ class DeduceProcessorLoader:  # pylint: disable=R0903
     def _add_recall_booster_arguments(
         recall_boost_config: dict, processor_args: dict
     ) -> None:
-        """Adds recall booster arguments to processor arguments"""
+        """Adds recall booster arguments to processor arguments."""
         if recall_boost_config["recall_boost_type"].endswith("MinimumLengthExpander"):
             str_processors = recall_boost_config["args"]["str_processors"]
             str_processors = [
