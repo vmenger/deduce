31907ed57b665e0c10ab94d6e9e67e557866f1b3
Lydia Mennes <lydia.mennes@nedap.com>
2024-03-12 10:40:41 +0100

    updated patient env list and start of name list comparison

--- deduce/try_wordlist_TEMP.py
+++ deduce/try_wordlist_TEMP.py
@@ -2,47 +2,54 @@ from pathlib import Path
 
 import dutch_words
 
-words = dutch_words.get_ranked()
 
-print("knop" in words)
+def common_word_list():
+    words = dutch_words.get_ranked()
 
-capital_split = {True: [], False: []}
-for w in reversed(words):
-    capital_split[w[0].isupper()].append(w)
+    print("knop" in words)
 
-print("Capitalized words:", len(capital_split[True]))
-print("Not capitalized words:", len(capital_split[False]))
+    capital_split = {True: [], False: []}
+    for w in reversed(words):
+        capital_split[w[0].isupper()].append(w)
 
-# for s, e in [(0, 10), (10, 20), (20, 30), (30, 40), (40, 50)]:
-#     print("Capitalized:", s, "to", e, "words:")
-#     for w in capital_split[True][s:e]:
-#         print(w)
-#     print("Not capitalized:", s, "to", e, "words:")
-#     for w in capital_split[False][s:e]:
-#         print(w)
-#     input("Press enter to continue")
+    print("Capitalized words:", len(capital_split[True]))
+    print("Not capitalized words:", len(capital_split[False]))
 
+    # for s, e in [(0, 10), (10, 20), (20, 30), (30, 40), (40, 50)]:
+    #     print("Capitalized:", s, "to", e, "words:")
+    #     for w in capital_split[True][s:e]:
+    #         print(w)
+    #     print("Not capitalized:", s, "to", e, "words:")
+    #     for w in capital_split[False][s:e]:
+    #         print(w)
+    #     input("Press enter to continue")
 
-used_common_word_list = []
-path = Path(
-    "/Users/lydia.mennes/code_base/deduce/deduce/data/lookup/src/whitelist/lst_common_word/items.txt"
-)
-with open(path, "r") as f:
-    for line in f:
-        used_common_word_list.append(line.strip())
+    used_common_word_list = []
+    path = Path("data/lookup/src/whitelist/lst_common_word/items.txt")
+    with open(path, "r") as f:
+        for line in f:
+            used_common_word_list.append(line.strip())
 
-diff_set = set(capital_split[False]) - set(used_common_word_list)
-print("\n\n=========diff common words:\n")
-for w in diff_set:
-    print(w)
-
-print(
-    f"\n\n=========diff common words count: {len(diff_set)} out of {len(capital_split[False])}\n"
-)
-
-output_path = Path(
-    "/Users/lydia.mennes/code_base/deduce/deduce/data/lookup/src/whitelist/lst_common_word/extra_items.txt"
-)
-with open(output_path, "w") as f:
+    diff_set = set(capital_split[False]) - set(used_common_word_list)
+    print("\n\n=========diff common words:\n")
     for w in diff_set:
-        f.write(w + "\n")
+        print(w)
+
+    print(
+        f"\n\n=========diff common words count: {len(diff_set)} out of {len(capital_split[False])}\n"
+    )
+
+    output_path = Path("/data/lookup/src/whitelist/lst_common_word/extra_items.txt")
+    with open(output_path, "w") as f:
+        for w in diff_set:
+            f.write(w + "\n")
+
+
+def names_list():
+    pass
+
+
+if __name__ == "__main__":
+    common_word_list()
+    # names_list()
+    pass
