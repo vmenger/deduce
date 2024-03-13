0cd779c067ec4bf725c3d1abbb701f9c1c94804e
Lydia Mennes <lydia.mennes@nedap.com>
2024-03-13 15:17:49 +0100

    test case finalized

--- tests/pipeline/test_lookupset_additions.py
+++ tests/pipeline/test_lookupset_additions.py
@@ -1,8 +1,8 @@
-def run_add_to_lookup_trie(model):
+def test_add_to_lookup_trie(model):
     example_dict = {
-        "first_name": (["Lydia", "Soeroedjpersad", "Loua", "Ümmü", "Saâdia"]),
-        "surname": ["Schoenmaker", "Schrijnewerkers", "Öcal", "Kayim"],
-        "placename": ["Rotterdam", "Pangkalpinang", "Riga", "Caïro"],
+        "first_name": (["Lydia", "Oempaloempa", "Saâdawajaia"]),
+        "surname": ["Schoenmaker", "Schrijnewerkers", "Öcalialent"],
+        "placename": ["Rotterdam", "Pangasiusfilet", "Caïro"],
     }
 
     for list_name, examples in example_dict.items():
@@ -16,6 +16,7 @@ def run_add_to_lookup_trie(model):
             model.lookup_structs[list_name].add_item([example])
             assert [example] in model.lookup_structs[list_name]
 
+    # Must rerun annotator loading in order for the additions to take effect
     model.processors = model.annotator_loader.load(
         config=model.config, extras=model.annotator_load_extras
     )
@@ -24,10 +25,3 @@ def run_add_to_lookup_trie(model):
         for example in examples:
             result = model.deidentify(sentence)
             assert len(result.annotations) == 1
-
-
-if __name__ == "__main__":
-    from deduce.deduce import Deduce
-
-    model = Deduce()
-    run_add_to_lookup_trie(model)
