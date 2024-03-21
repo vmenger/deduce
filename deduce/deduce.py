44b4a1a5347c49e9b043789f56ae16a5e7bbb3fc
Tom Welter <t.g.welter@amsterdamumc.nl>
2024-03-21 20:56:31 +0100

    Add configurable path to lookup tables
    Add key "lookup_table_path" to config.json that points to the
    directory where 'cache' en 'src' are located.
    Path is absolute ('C:/data/base-config.json' , '/data/baseconfig.json')
    or relative to the location of the base-config.json (up or down the tree
    e.g. './data/lookup' or '../../configs/version-1')
    
    Also changed the 'all_lists' variable definition which pointed to a
    fixed location in deduce. 'all_lists' is now also placed in the config
    file as key "all_lists".
    
    -Does not break previous configuration settings
    -Priority order:
    1) use values from base-config.json
    2) when not found in config.json, use old default behavior
    3) when old version of all_lists AND the one from config.json are '[]',
    generate all _lists dynamically from directory structure.
    
    base-config.json:
    {
      "lookup_table_path": "./data/lookup",
      "all_lists": [
        "institutions/lst_healthcare_institution",
        "institutions/lst_hospital",
        "institutions/lst_hospital_abbr",
        "locations/lst_placename",
        "locations/lst_street",
        "names/lst_first_name",
        "names/lst_initial",
        "names/lst_interfix",
        "names/lst_interfix_surname",
        "names/lst_prefix",
        "names/lst_surname",
        "whitelist/lst_common_word",
        "whitelist/lst_eponymous_disease",
        "whitelist/lst_medical_term",
        "whitelist/lst_stop_word"
      ],
      "adjacent_annotations_slack": "[\\. \\-]?[\\. ]?",
      ...
      [snip]
      ...
      }

--- deduce/deduce.py
+++ deduce/deduce.py
@@ -26,6 +26,7 @@ from deduce.lookup_struct_loader import load_interfix_lookup, load_prefix_lookup
 from deduce.lookup_structs import get_lookup_structs, load_raw_itemsets
 from deduce.redactor import DeduceRedactor
 from deduce.tokenizer import DeduceTokenizer
+from deduce.data.lookup.src import all_lists
 
 __version__ = importlib.metadata.version(__package__ or __name__)
 
@@ -55,7 +56,7 @@ class Deduce(dd.DocDeid):  # pylint: disable=R0903
             are overwritten, and other defaults are kept. When `load_base_config` is
             set to `False`, no defaults are loaded and only configuration from `config`
             is applied.
-        looup_data_path: The path to look for lookup data, by default included in
+        lookup_data_path: The path to look for lookup data, by default included in
             the package. If you want to make changes to source files, it's recommended
             to copy the source data and pointing deduce to this folder with this
             argument.
@@ -71,6 +72,8 @@ class Deduce(dd.DocDeid):  # pylint: disable=R0903
         lookup_data_path: Union[str, Path] = _LOOKUP_LIST_PATH,
         build_lookup_structs: bool = False,
     ) -> None:
+
+        global all_lists
 
         super().__init__()
 
@@ -88,12 +91,26 @@ class Deduce(dd.DocDeid):  # pylint: disable=R0903
             load_base_config=load_base_config, user_config=config
         )
 
-        self.lookup_data_path = self._initialize_lookup_data_path(lookup_data_path)
+        if "lookup_table_path" in self.config.keys():
+            config_file_path = Path(os.path.dirname(Path(self.config["config_file_dir"])))
+            self.lookup_data_path = config_file_path.joinpath(Path(self.config["lookup_table_path"]))
+        else:
+            self.lookup_data_path = Path(self._initialize_lookup_data_path(lookup_data_path))
+        logging.info("Loading lookup data structures from: '" + str(self.lookup_data_path.absolute()) + "'.")
         self.tokenizers = {"default": self._initialize_tokenizer(self.lookup_data_path)}
-
+        
+        if "all_lists" in self.config.keys():
+            all_lists=self.config["all_lists"]
+        if len(all_lists) == 0:
+            # generate a new one if deduce.data.lookup.src.all_lists is empty AND it is empty/not present in config.json
+            all_lists=[]
+            for i in self.lookup_data_path.glob("src/*/lst_*"):
+                all_lists.append( os.path.basename(os.path.split(i)[0]) + "/" + os.path.basename(i))             
+       
         self.lookup_structs = get_lookup_structs(
-            lookup_path=self.lookup_data_path,
+            lookup_path=Path(os.path.realpath(self.lookup_data_path)),
             tokenizer=self.tokenizers["default"],
+            all_lists=all_lists,
             deduce_version=__version__,
             build=build_lookup_structs,
         )
@@ -124,9 +141,12 @@ class Deduce(dd.DocDeid):  # pylint: disable=R0903
                 base_config = json.load(file)
 
             utils.overwrite_dict(config, base_config)
+            # store the config-file-dir as an entry in the config dict
+            config["config_file_dir"] = _BASE_CONFIG_FILE
 
         if user_config is not None:
             if isinstance(user_config, str):
+                config["config_file_dir"] = user_config
                 with open(user_config, "r", encoding="utf-8") as file:
                     user_config = json.load(file)
 
@@ -171,7 +191,7 @@ class _DeduceProcessorLoader:  # pylint: disable=R0903
             args.update(
                 lookup_values=lookup_struct.items(),
                 matching_pipeline=lookup_struct.matching_pipeline,
-                tokenizer=extras["tokenizer]"],
+                tokenizer=extras["tokenizer"],
             )
         elif isinstance(lookup_struct, dd.ds.LookupTrie):
             args.update(trie=lookup_struct)
